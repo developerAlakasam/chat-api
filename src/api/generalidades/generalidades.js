53606dee86f79d70c934aca07853e5b95f152b5f
developerAlakasam <developeralakasam@gmail.com>
2023-04-24 09:51:22 -0600

    lista de marcas con generalidades

--- src/api/generalidades/generalidades.js
+++ src/api/generalidades/generalidades.js
@@ -2,10 +2,19 @@ import { connection } from "../../database.js";
 
 export const getGeneralidades = async (req, res) => {
   const brand = req.params.brand;
-  connection.query(
-    "SELECT * FROM generalidades WHERE keyword = '" + brand + "'",
-    function (err, results, fields) {
-        return res.status(200).json({ results });
-    }
-  );
+  if(brand=='list'){
+    connection.query(
+      "SELECT keyword FROM generalidades",
+      function (err, results, fields) {
+          return res.status(200).json({ results });
+      }
+    );
+  }else{
+    connection.query(
+      "SELECT * FROM generalidades WHERE keyword = '" + brand + "'",
+      function (err, results, fields) {
+          return res.status(200).json({ results });
+      }
+    );
+  }
 };
